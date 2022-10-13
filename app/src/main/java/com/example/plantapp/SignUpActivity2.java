71d688946dbb04a1df530f4bb1a62e2e04337c38
Rikky250503 <113768806+Rikky250503@users.noreply.github.com>
2022-10-13 21:45:05 +0700

    PlantApps

---
+++ app/src/main/java/com/example/plantapp/SignUpActivity2.java
@@ -0,0 +1,16 @@
+package com.example.plantapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2);
+    }
+}
